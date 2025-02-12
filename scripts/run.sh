ccaf22d1f296dd6c7fb6d74fb99ea738ef621ec1
Ayush Kumar <ayushkumar@Ayushs-Laptop.local>
2025-02-13 01:52:50 +0530

    prod

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,4 +8,4 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Starting Gunicorn server..."
-gunicorn your_project_name.wsgi:application --bind 0.0.0.0:8000
+gunicorn app.wsgi:application --bind 0.0.0.0:8000
